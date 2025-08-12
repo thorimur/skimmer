f9dd3c0b25923bd2b6637ec9fdde836d2f986522
thorimur <68410468+thorimur@users.noreply.github.com>
2025-08-12 14:02:29 -0400

    EditTest.lean

---
+++ EditTest.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `EditTest` library.
+-- Import modules here that should be built as part of the library.
+import EditTest.Extension
+import EditTest.Utilities
