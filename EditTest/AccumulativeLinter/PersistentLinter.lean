c912de3bec4dd39b16dbfb08a8ba1185ddf1f719
thorimur <thomasmurrills@gmail.com>
2025-09-22 12:53:51 -0400

    AccumulativeLinter, SimpleAccumulativeLinter

--- EditTest/AccumulativeLinter/PersistentLinter.lean
+++ EditTest/AccumulativeLinter/PersistentLinter.lean
@@ -2,6 +2,20 @@ import EditTest.LinterWithCleanup.Run
 
 /-!
 We provide a way to construct a `LinterWithCleanup` that handles an `IO.Ref`.
+
+```lean
+initialize myRef : IO.Ref κ ← IO.mkRef init
+
+def myPLinter : PersistentLinter ρ κ where
+  ref           := myRef
+  add r k       := ...
+  persist k env := ...
+  produce? stx  := ...
+  runOnEOI      := ...
+  runOnHeader   := ...
+
+initialize addPersistentLinter myPLinter
+```
 -/
 
 open Lean Elab Command
@@ -25,3 +39,59 @@ def PersistentLinter.toLinterWithCleanup (l : PersistentLinter ρ κ) : LinterWi
     modifyEnv (l.persist k)
   runOnEOI    := l.runOnEOI
   runOnHeader := l.runOnHeader
+
+def addPersistentLinter (l : PersistentLinter ρ κ) : IO Unit := addLinterWithCleanup l.toLinterWithCleanup
+
+-- Include `*Descr` versions? Not really necessary, just saving an `initialize myRef`. But makes `AccumulativeLinterDescr` nicer.
+
+
+/-! ### AccumulativeLinter -/
+
+structure AccumulativeLinterDescr (α β σ ρ κ) extends PersistentEnvExtensionDescr α β σ where
+  -- Part of what can be thought of as `PersistentRefDescr`, but we fix a factor of `persist` and leave free only the factor that yields the updates
+  init : κ
+  add : ρ → κ → κ
+  submit : κ → Array β
+  -- Linter
+  produce? : Syntax → CommandElabM (Option ρ)
+  runOnEOI : CommandElabM Bool := pure true
+  runOnHeader : CommandElabM Bool := pure false
+
+structure AccumulativeLinter (α β σ ρ κ) extends PersistentLinter ρ κ where
+  ext : PersistentEnvExtension α β σ
+
+def registerAndAddAccumulativeLinter [Inhabited σ] (a : AccumulativeLinterDescr α β σ ρ κ) : IO (AccumulativeLinter α β σ ρ κ) := do
+  let ref ← IO.mkRef a.init
+  let ext ← registerPersistentEnvExtension a.toPersistentEnvExtensionDescr
+  let persistentLinter : PersistentLinter ρ κ := { a with
+    ref
+    persist k env := Id.run do
+      let bs := a.submit k
+      if bs.isEmpty then return env else
+        let mut env := env
+        for b in bs do
+          env := ext.addEntry env b -- do we need to allow the descr to set async args?
+        return env
+  }
+  addPersistentLinter persistentLinter
+  return { persistentLinter with ext }
+
+/-! ### SimpleAccumulativeLinter -/
+
+def SimpleAccumulativeLinter (α β σ) := AccumulativeLinter α β σ β (Array β)
+
+structure SimpleAccumulativeLinterDescr (α β σ) extends PersistentEnvExtensionDescr α β σ where
+  produce? : Syntax → CommandElabM (Option β)
+  runOnEOI : CommandElabM Bool := pure true
+  runOnHeader : CommandElabM Bool := pure false
+
+def SimpleAccumulativeLinterDescr.toAccumulativeLinterDescr (s : SimpleAccumulativeLinterDescr α β σ) :
+    AccumulativeLinterDescr α β σ β (Array β) := { s with
+  init := #[]
+  add b bs := bs.push b
+  submit := id
+}
+
+def registerAndAddSimpleAccumulativeLinter [Inhabited σ] (s : SimpleAccumulativeLinterDescr α β σ) :
+    IO (SimpleAccumulativeLinter α β σ) :=
+  registerAndAddAccumulativeLinter s.toAccumulativeLinterDescr
