91cdf3c2f25867386af8b8ab26f57a2b53d285af
thorimur <thomasmurrills@gmail.com>
2025-09-22 11:26:30 -0400

    PunchCard (simplify design)

--- EditTest/AccumulativeLinter/PunchCard.lean
+++ EditTest/AccumulativeLinter/PunchCard.lean
@@ -6,41 +6,34 @@ TODO: pick a better metaphor?
 -/
 
 inductive PunchCard where
-| unfinished | finished | waitedOn (c : Std.Condvar)
+| unfinished | finished | waitedOn
 deriving Inhabited
+
+@[inline] def PunchCard.isFinished : PunchCard → Bool
+  | .finished => true
+  | _         => false
 
 /-- This should only be pushed to by `addLinterWithCleanup`. -/
 initialize punchCardsRef : IO.Ref (Array PunchCard) ← IO.mkRef #[]
 
+/- We potentially read the reference more times than we'd like with this, but that shouldn't be awful, especially since in most cases there is only one `waitedOn` at a time. -/
+
+/-- This bell is rung each time something which has been `waitedOn` finishes. -/
+initialize punchCardBell : Std.Condvar ← Std.Condvar.new
+
 -- !! Should we have a global mutex like this, or make a new mutex each time we wait?
 initialize punchCardWaiter : Std.BaseMutex ← Std.BaseMutex.new
 
-def IO.waitForPunched (idx : Nat) : BaseIO Unit := do
-  match (← punchCardsRef.get)[idx]! with
-  | .finished => return
-  | .waitedOn c =>
-    punchCardWaiter.lock -- !! Necessary?
-    c.wait punchCardWaiter
-    punchCardWaiter.unlock
-  | .unfinished => do
-    let c ← Std.Condvar.new
-    punchCardWaiter.lock -- !! Necessary?
-    let some c ← punchCardsRef.modifyGet fun a =>
-      a.modifyGet idx fun
-        | .finished => (none, .finished)
-        | w@(.waitedOn c) => (some c, w)
-        | .unfinished => (some c, .waitedOn c)
-      | punchCardWaiter.unlock
-    c.wait punchCardWaiter
-    punchCardWaiter.unlock
+def IO.waitForPunched! (idx : Nat) : BaseIO Unit := do
+  punchCardWaiter.lock
+  punchCardBell.waitUntil punchCardWaiter <| return (← punchCardsRef.get)[idx]!.isFinished
+  punchCardWaiter.unlock
 
 def IO.punch! (idx : Nat) : BaseIO Unit := do
-  let some c ← punchCardsRef.modifyGet fun a =>
-    if let .waitedOn c := a[idx]! then
-      (some c, a.set! idx .finished)
-    else
-      (none, a.set! idx .finished)
-    | return
-  punchCardWaiter.lock -- !! correct?
-  punchCardWaiter.unlock
-  c.notifyAll
+  let shouldRingBell ← punchCardsRef.modifyGet fun a =>
+    match a[idx]! with
+    | .waitedOn   => (true,  a.set! idx .finished)
+    | .unfinished => (false, a.set! idx .finished)
+    | .finished   => (false, a)
+  -- We lock the mutex in case we'd be notifying while the predicate is being checked, before we re-wait.
+  if shouldRingBell then punchCardWaiter.lock; punchCardBell.notifyAll; punchCardWaiter.unlock
