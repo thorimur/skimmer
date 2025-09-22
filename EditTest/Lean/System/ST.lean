8051034c3f43cdb24db991bc636621267ca563ff
thorimur <68410468+thorimur@users.noreply.github.com>
2025-09-22 10:30:29 -0400

    ST (modify(Get)M)

---
+++ EditTest/Lean/System/ST.lean
@@ -0,0 +1,42 @@
+import Lean
+
+namespace ST
+
+variable {σ : Type} {m : Type → Type} [Monad m] [MonadLiftT (ST σ) m]
+
+namespace Prim
+
+@[inline] unsafe def Ref.modifyMUnsafe {α : Type} (r : Ref σ α) (f : α → m α) : m Unit := do
+  let v ← Ref.take r
+  let v ← f v
+  Ref.set r v
+
+@[inline] unsafe def Ref.modifyGetMUnsafe {α β : Type} (r : Ref σ α) (f : α → m (β × α)) : m β := do
+  let v ← Ref.take r
+  let (b, a) ← f v
+  Ref.set r a
+  pure b
+
+@[implemented_by Ref.modifyMUnsafe]
+def Ref.modifyM {α : Type} (r : Ref σ α) (f : α → m α) : m Unit := do
+  let v ← Ref.get r
+  let v ← f v
+  Ref.set r v
+
+@[implemented_by Ref.modifyGetMUnsafe]
+def Ref.modifyGetM {α β : Type} (r : Ref σ α) (f : α → m (β × α)) : m β := do
+  let v ← Ref.get r
+  let (b, a) ← f v
+  Ref.set r a
+  pure b
+
+end Prim
+
+@[inline] def Ref.modifyM {α : Type} (r : Ref σ α) (f : α → m α) : m Unit :=
+  liftM <| Prim.Ref.modifyM r f
+/--
+Atomically modifies a mutable reference cell by replacing its contents with the result of a function
+call that simultaneously computes a value to return.
+-/
+@[inline] def Ref.modifyGetM {α : Type} {β : Type} (r : Ref σ α) (f : α → m (β × α)) : m β :=
+  liftM <| Prim.Ref.modifyGetM r f
