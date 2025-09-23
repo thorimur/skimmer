9e241c0c17fc91e30fe782db85de7f13a781637c
thorimur <68410468+thorimur@users.noreply.github.com>
2025-09-23 12:47:03 -0400

    update `Refactor` linter

--- Skimmer/Refactor/AccumulativeLinter.lean
+++ Skimmer/Refactor/AccumulativeLinter.lean
@@ -1,22 +1,20 @@
 import Skimmer.Refactor.Attr
-import Skimmer.Accumulator.Defs
+import Skimmer.AccumulativeLinter
 
 open Lean
 
-
-def commandRefactorDescr : AccumulatorDescr (Array Edit) Edit SourceIndexedArray IO.Ref where
-  name := `commandRefactor
-  recordInLinter? stx := do
-    let refactorss := commandRefactorAttr.ext.getImportedEntries (← getEnv)
-    let mut edits := #[]
-    for refactors in refactorss do
-      for refactor in refactors do
-        let refactor ← unsafe evalConstCheck Refactor ``Refactor refactor
-        edits := edits ++ (← refactor stx)
-    if edits.isEmpty then return none else return some edits
-  append a edits := a ++ edits.2
-  exportEntriesFnEx _ a _ := a.sortEdits
-  interactiveTrackingScope := .upToCommandEndWithTrailing
-
-initialize commandRefactor : Accumulator (Array Edit) Edit Edit SourceIndexedArray IO.Ref ←
-  commandRefactorDescr.registerAccumulator
+initialize commandRefactor : SimpleAccumulativeLinter Edit (Array Edit) (Array Edit) ←
+  registerAndAddSimpleAccumulativeLinter {
+    mkInitial := pure #[]
+    addImportedFn := fun _ => pure #[]
+    addEntryFn := Array.append
+    produce? stx := do
+      let refactorss := commandRefactorAttr.ext.getImportedEntries (← getEnv)
+      let mut edits := #[]
+      for refactors in refactorss do
+        for refactor in refactors do
+          let refactor ← unsafe evalConstCheck Refactor ``Refactor refactor
+          edits := edits ++ (← refactor stx)
+      if edits.isEmpty then return none else return some edits
+    exportEntriesFnEx _ edits _ := edits.sortEdits
+  }
