5474b264692838f522f26e2ac3e37efe817222ba
thorimur <68410468+thorimur@users.noreply.github.com>
2026-01-21 12:45:30 -0500

    .

--- Skimmer/Dive.lean
+++ Skimmer/Dive.lean
@@ -17,5 +17,5 @@ elab_rules : command
 | `(command|dive%$tk) => liftCoreM <| do
     addSuggestion tk (header := "Prepare the following actions?\n\n  refactor deprecated all") (s := { suggestion := .string "prepare" })
 | `(command|dive%$tk prepare%$p) => liftCoreM <| do
-    addSuggestion (mkNullNode #[tk, p]) (header := "Prepared 6 refactors, 1 of which needs review. Apply refactors? the following dives?\n\n  refactor deprecated all") (s := { suggestion := .string "execute" })
+    addSuggestion (mkNullNode #[tk, p]) (header := "Prepared 6 refactors, 1 of which needs review. Apply refactors?") (s := { suggestion := .string "execute" })
 | `(command|dive prepare execute) => pure ()
