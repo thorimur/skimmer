3533bb17d392fc556187719b8f433188a41fb260
thorimur <thomasmurrills@gmail.com>
2026-02-01 19:02:56 -0500

    chore: handle edits in Main

--- Skimmer/Working/Main.lean
+++ Skimmer/Working/Main.lean
@@ -1,6 +1,7 @@
 module
 
 import Skimmer.Working.Cruft
+import Skimmer.Refactor.Util.Ident
 
 def sourceFileDummy := "\
 import Lean
@@ -37,20 +38,20 @@ variable (x : EIO Exception Bool)
 -- TODO: should build on top of this to grab messages and such, I think that's what runAndReport does? would runAndReport fill in syntax/pos?
 
 def Lean.Language.Lean.CommandParsedSnapshot.EIO.runCommandElabM (ictx : Parser.InputContext)
-    (x : CommandElabM α) (snap : CommandParsedSnapshot) : EIO Exception (α × State) := do
-  x.run (snap.toCommandCtx ictx) |>.run (snap.elabSnap.resultSnap.get.cmdState)
+    (x : Syntax → CommandElabM α) (snap : CommandParsedSnapshot) : EIO Exception (α × State) := do
+  x snap.getSyntax |>.run (snap.toCommandCtx ictx) |>.run (snap.elabSnap.resultSnap.get.cmdState)
 
 def Lean.Language.Lean.CommandParsedSnapshot.EIO.runCommandElabM' (ictx : Parser.InputContext)
-    (x : CommandElabM α) (snap : CommandParsedSnapshot) : EIO Exception α := do
-  x.run (snap.toCommandCtx ictx) |>.run' (snap.elabSnap.resultSnap.get.cmdState)
+    (x : Syntax → CommandElabM α) (snap : CommandParsedSnapshot) : EIO Exception α := do
+  x snap.getSyntax |>.run (snap.toCommandCtx ictx) |>.run' (snap.elabSnap.resultSnap.get.cmdState)
 
 def Lean.Language.Lean.CommandParsedSnapshot.runCommandElabM (ictx : Parser.InputContext)
-    (x : CommandElabM α) (snap : CommandParsedSnapshot) : IO (Except Exception (α × State)) := do
-  x.run (snap.toCommandCtx ictx) |>.run (snap.elabSnap.resultSnap.get.cmdState) |>.toIO'
+    (x : Syntax → CommandElabM α) (snap : CommandParsedSnapshot) : IO (Except Exception (α × State)) := do
+  x snap.getSyntax |>.run (snap.toCommandCtx ictx) |>.run (snap.elabSnap.resultSnap.get.cmdState) |>.toIO'
 
 def Lean.Language.Lean.CommandParsedSnapshot.runCommandElabM' (ictx : Parser.InputContext)
-    (x : CommandElabM α) (snap : CommandParsedSnapshot) : IO (Except Exception α) := do
-  x.run (snap.toCommandCtx ictx) |>.run' (snap.elabSnap.resultSnap.get.cmdState) |>.toIO'
+    (x : Syntax → CommandElabM α) (snap : CommandParsedSnapshot) : IO (Except Exception α) := do
+  x snap.getSyntax |>.run (snap.toCommandCtx ictx) |>.run' (snap.elabSnap.resultSnap.get.cmdState) |>.toIO'
 
 
 #check Lean.Language.Lean.process
@@ -70,9 +71,11 @@ def Lean.Language.Lean.CommandParsedSnapshot.runCommandElabM' (ictx : Parser.Inp
 
 -- Given a `usage : Syntax`, we need to get the info from the infotree of *the part that the ident looks at* for the expected type. This depends I think. Then we get the expected type, make replacements, see if it all works out.
 
+def _root_.String.Pos.Raw.rangeAt (pos : String.Pos.Raw) : Syntax.Range := ⟨pos, pos⟩
+
 -- instance : Repr Language.Lean.CommandParsedSnapshot where
 --   reprPrec snap _ := s!"{snap.desc}: [{snap.stx}] {snap.elabSnap.elabSnap.get}"
-
+open Skimmer in
 public def main : IO Unit := do
   initSearchPath (← findSysroot)
   let inputCtx := Parser.mkInputContext sourceFileDummy "<dummy>"
@@ -85,19 +88,25 @@ public def main : IO Unit := do
   -- read into `replacements`.
   IO.println s!"{repr r.stx.raw.getRangeWithTrailing?}; {headerParserState.pos}"
   let mut replacements : NameMap Name := {}
-  let mut edits : Array String := #[] -- import actual `Edit` functionality here
+  let mut edits : Array Edit := #[] -- import actual `Edit` functionality here
   for snap in commands do
     -- TODO: could be useful to run in new state, but this is really a subprocess-version problem.
-    match ← snap.runCommandElabM' inputCtx <| replaceDeprecated replacements edits with
+    match ← snap.runCommandElabM' inputCtx <| refactorDeprecated.post replacements edits with
     | .error ex => IO.eprintln (← ex.toMessageData.toString)
     | .ok (replacements', edits') =>
       replacements := replacements'
       edits := edits'
-    IO.println s!"----\nedits: {edits}\n{snap.getSyntax.reprint.getD "couldn't reprint"}"
+    edits := edits.push ⟨snap.getSyntax.getPos?.get!.rangeAt, "aaa!", true⟩
+    IO.println s!"----\nedits: {repr edits}\n{snap.getSyntax.reprint.getD "couldn't reprint"}"
   -- finally write olean.skimmed. don't bother with error handling yet
-where
-  replaceDeprecated (r : NameMap Name) (e : Array String) :=
-    return (r, e.push s!"another! (foo exists: {(← getEnv).find? `foo |>.isSome})")
+  -- TODO standardize edits postprocessing as part of what "edits" are. this should be an extensible part of introducing accumulation
+  if edits.any (·.shouldReview) then
+    edits := edits.push ⟨headerParserState.pos.rangeAt, "\nimport Skimmer.Review\n", false⟩
+  edits := edits.qsortOrd
+  IO.println s!"====\nedits: {repr edits}"
+  IO.println s!"====\n{sourceFileDummy.applyEdits edits}"
+  -- replaceDeprecated (r : NameMap Name) (e : Array String) :=
+  --   return (r, e.push s!"another! (foo exists: {(← getEnv).find? `foo |>.isSome})")
 /-
 Rewrite write-edits:
 
