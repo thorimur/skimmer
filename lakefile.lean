9a8880085bcae0697db26da62b41c4a437aa1d8c
thorimur <thomasmurrills@gmail.com>
2026-02-02 01:33:24 -0500

    Revert "chore: update lakefile"
    This reverts commit aa32dc9540d93c884fb620928c9d6b5540d4ed98.

--- lakefile.lean
+++ lakefile.lean
@@ -21,15 +21,11 @@ require "leanprover-community" / batteries @ git "main"
   globs := #[`SkimmerExtra.+]
   leanOptions := #[⟨`experimental.module, true⟩]
 
-@[default_target] lean_lib SkimmerWorking where
-  globs := #[`SkimmerWorking.+]
-  leanOptions := #[⟨`experimental.module, true⟩]
-
 @[default_target] lean_exe write_edits where
   root := `Skimmer.Execute
   leanOptions := #[⟨`experimental.module, true⟩]
 
 @[default_target] lean_exe working where
-  root := `SkimmerWorking.Main
+  root := `Skimmer.Working.Main
   supportInterpreter := true
   leanOptions := #[⟨`experimental.module, true⟩]
