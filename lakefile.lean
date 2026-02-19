a49cdc6cbd0b59798ab7d22a6a1d55537c634a2c
thorimur <68410468+thorimur@users.noreply.github.com>
2026-02-18 23:10:44 -0500

    temp: thinking

--- lakefile.lean
+++ lakefile.lean
@@ -1,4 +1,5 @@
 import Lake
+import Lean
 
 open System Lake DSL
 
@@ -37,3 +38,22 @@ require "leanprover-community" / batteries @ git "main"
 lean_lib WorkingTest where
   globs := #[`WorkingTest.+]
   leanOptions := #[⟨`experimental.module, true⟩]
+
+section
+
+open Lean
+
+structure EditData where
+  edits : System.FilePath
+  mdata : System.FilePath
+  -- The following have to be pickled into the mdata, in order to cross the subprocess boundary
+  -- The artifact consists of both of these together, of course. Can we have subartifacts?
+  -- Further on we may want edits to be contributed to by multiple things; will we want to aggregate the mdata on the fly? Probably produce an aggregated mdata via pickle
+  -- uri : Lsp.DocumentUri
+  -- editLocs   : Array Lsp.Range
+  -- reviewLocs : Array Lsp.Range
+
+end
+
+module_facet pickleJar (mod : Module) : EditData := do
+  let e ← fetch mod.backend
