40b6440110caa1cd6f7067576e679b2f4e3d2993
thorimur <68410468+thorimur@users.noreply.github.com>
2026-03-02 01:23:17 -0500

    chore: disable unnecessary targets

--- lakefile.lean
+++ lakefile.lean
@@ -8,27 +8,25 @@ open Lake DSL
 package skimmer where version := v!"0.1.0"
 
 require "leanprover-community" / batteries @ git "main"
-require "leanprover" / "doc-gen4" @ git "main"
-
 
 @[default_target] lean_lib Skimmer where leanOptions := #[⟨`experimental.module, true⟩]
 
-@[default_target] lean_lib SkimmerPlugin where
-  globs := #[`SkimmerPlugin.+]
-  defaultFacets := #[`lean_lib.shared]
-  leanOptions := #[⟨`experimental.module, true⟩]
+-- @[default_target] lean_lib SkimmerPlugin where
+--   globs := #[`SkimmerPlugin.+]
+--   defaultFacets := #[`lean_lib.shared]
+--   leanOptions := #[⟨`experimental.module, true⟩]
 
-@[default_target] lean_lib SkimmerTest where
-  globs := #[`SkimmerTest.+]
-  leanOptions := #[⟨`experimental.module, true⟩]
+-- @[default_target] lean_lib SkimmerTest where
+--   globs := #[`SkimmerTest.+]
+--   leanOptions := #[⟨`experimental.module, true⟩]
 
-@[default_target] lean_lib SkimmerExtra where
-  globs := #[`SkimmerExtra.+]
-  leanOptions := #[⟨`experimental.module, true⟩]
+-- @[default_target] lean_lib SkimmerExtra where
+--   globs := #[`SkimmerExtra.+]
+--   leanOptions := #[⟨`experimental.module, true⟩]
 
-@[default_target] lean_lib SkimmerHub where
-  globs := #[`SkimmerExtra.+]
-  leanOptions := #[⟨`experimental.module, true⟩]
+-- @[default_target] lean_lib SkimmerHub where
+--   globs := #[`SkimmerExtra.+]
+--   leanOptions := #[⟨`experimental.module, true⟩]
 
 @[default_target] lean_exe write_edits where
   root := `Skimmer.Execute
