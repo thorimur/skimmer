8ca57729bcd05a79ad7b91e801e7279dd83a4446
thorimur <68410468+thorimur@users.noreply.github.com>
2026-03-13 20:11:58 -0400

    chore: move back to `bindM`?

--- lakefile.lean
+++ lakefile.lean
@@ -244,9 +244,10 @@ def Lake.Module.refactorWithExe
     (setupFile : System.FilePath)
     (importArts : Array System.FilePath) (mod : Lake.Module) :
     JobM (Job System.FilePath) := do
-    let leanJob ← mod.lean.fetch
-    discard leanJob.await
-    addTrace leanJob.getTrace
+  (← mod.lean.fetch).bindM fun _ => do
+    -- let leanJob ← mod.lean.fetch
+    -- discard leanJob.await
+    -- addTrace leanJob.getTrace
     let args := mod.mkRefactorArgs recordRefactorFacet setupFile importArts
     (← fetchExeSpawnArgs refactorExe #[(toJson args).compress]).mapM fun spawnArgs => do
       discard <| buildArtifactUnlessUpToDate (text := true) args.buildFile do
